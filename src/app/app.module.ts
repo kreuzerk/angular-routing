776c81f36276273957d8002d7cce72e439e76763
kreuzerk <kevin.kreuzer90@icloud.com>
2017-12-19 08:50:59 +0100

    Add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 
 // Imports for loading & configuring the in-memory web api
@@ -15,18 +14,18 @@ import {PageNotFoundComponent} from './page-not-found.component';
 import {ProductModule} from './products/product.module';
 import {UserModule} from './user/user.module';
 import {MessageModule} from './messages/message.module';
-import {appRoutes} from './app.routes';
 import {NavbarComponent} from './navbar/navbar.component';
+import {AppRoutingModule} from './app.routing.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         HttpClientModule,
-        RouterModule.forRoot(appRoutes),
         InMemoryWebApiModule.forRoot(ProductData, {delay: 1000}),
         ProductModule,
         UserModule,
-        MessageModule
+        MessageModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,9 +1,19 @@
 import {WelcomeComponent} from './home/welcome.component';
 import {PageNotFoundComponent} from './page-not-found.component';
-import {Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
 
-export const appRoutes: Routes = [
+const appRoutes: Routes = [
     {path: 'welcome', component: WelcomeComponent},
     {path: '', redirectTo: 'welcome', pathMatch: 'full'},
     {path: '**', component: PageNotFoundComponent},
-]
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
