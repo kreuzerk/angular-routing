61cab8fe247963d51bf6701e91c6fcdaf5e6e1f9
kreuzerk <kevin.kreuzer90@icloud.com>
2017-12-24 18:02:55 +0100

    Add and preserve query parameters

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 import {IProduct} from './product';
 import {ProductService} from './product.service';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
     templateUrl: './product-list.component.html',
@@ -17,7 +18,8 @@ export class ProductListComponent implements OnInit {
 
     products: IProduct[];
 
-    constructor(private productService: ProductService) {
+    constructor(private productService: ProductService,
+                private activatedRoute: ActivatedRoute) {
     }
 
     toggleImage(): void {
@@ -25,6 +27,14 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
+
+        this.activatedRoute.queryParamMap.subscribe(
+            (queryParams: Params) => {
+                this.listFilter = queryParams.get('filterBy');
+                this.showImage = queryParams.get('showImage') === 'true';
+            }
+        );
+
         this.productService.getProducts()
             .subscribe(products => this.products = products,
                 error => this.errorMessage = <any>error);
